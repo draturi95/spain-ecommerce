97c0f3c88f825129d6428b2c148a30d5b0b57c12
Dhawal Raturi <dhawalraturi@Dhawals-MacBook-Pro.local>
2022-06-19 17:29:14 +0530

    created: Seperated components for Product, Cart, FooterBanner, HeroBanner, Layout, Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,54 @@
-import React from 'react'
+  import React from 'react'
 
-const Home = () => {
-  return (
-    <>
-      Hero Banner 
+  import { client } from '../lib/client'
 
-      <div className='products-heading'>
-         <h2>Best Selling Products </h2>
+  import { Product, FooterBanner, HeroBanner } from '../components/'
 
-         <p>Speakers of many variations </p>
-      </div>
+  const Home = ({ productsData, bannerData }) => {
+    return (
+      <>
+        <HeroBanner heroBanner={bannerData.length ? bannerData[0] : null}/>
 
-      <div className='products-container'>
-        {
-          ["Produc1", "Product2"]
-          .map(
-            product => product
-          )
-        }
-      </div>
-    </>
-  )
-}
+       { console.log(bannerData)}
 
-export default Home
+        <div className='products-heading'>
+          <h2>Best Selling Products </h2>
+
+          <p>Speakers of many variations </p>
+        </div>
+
+        <div className='products-container'>
+          {
+            productsData
+            ?.map(
+              product => (
+                <Product key={product._id} product={product}/>
+              )
+            )
+          }
+        </div>
+
+        <FooterBanner/>
+      </>
+    )
+  }
+
+  export default Home; 
+
+
+  export const getServerSideProps = async() => {
+    const productQuery = '*[_type == "product"]'; 
+    const productsData =  await client.fetch(productQuery); 
+
+    const bannerQuery = '*[_type == "banner"]'; 
+    const bannerData =  await client.fetch(bannerQuery); 
+
+
+    return {
+      props: {
+        productsData, bannerData
+      }
+    }
+
+
+  }
